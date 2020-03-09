f3216ca5b93fcd6a21d579e043d82f443e42dffa
travis skyles <travis.skyles@gmail.com>
2020-03-08 23:07:09 -0700

    fixed enzyme configure

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,3 @@
-import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-Enzyme.config({ adapter: new Adapter() });
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
